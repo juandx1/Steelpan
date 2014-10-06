0e39c6598532ead07e96a09427d942dabb35e85d
juandx1 <juandx1@hotmail.com>
2014-10-06 14:02:25 -0500

    Adelanto logo, Actividad principal, Actividad Seleccion

---
+++ app/src/main/java/com/stigmasoft/matap/steelpan/SelectActivity.java
@@ -0,0 +1,16 @@
+package com.stigmasoft.matap.steelpan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Juan Manuel on 04/10/2014.
+ */
+public class SelectActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_activity);
+    }
+}
