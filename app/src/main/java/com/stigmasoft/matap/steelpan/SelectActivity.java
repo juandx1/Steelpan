379dd05f13cd18d767e9895ce21e3917fea0de55
juandx1 <juandx1@hotmail.com>
2014-10-09 18:29:37 -0500

    Tab implementados

--- app/src/main/java/com/stigmasoft/matap/steelpan/SelectActivity.java
+++ app/src/main/java/com/stigmasoft/matap/steelpan/SelectActivity.java
@@ -1,16 +1,77 @@
 package com.stigmasoft.matap.steelpan;
 
-import android.app.Activity;
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
+import android.view.MenuItem;
 
 /**
  * Created by Juan Manuel on 04/10/2014.
  */
-public class SelectActivity extends Activity {
+public class SelectActivity extends FragmentActivity implements ActionBar.TabListener{
+
+    private SelectSectionAdapter mAppSectionsPagerAdapter;
+
+    private ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_activity);
+
+
+        mAppSectionsPagerAdapter = new SelectSectionAdapter(getSupportFragmentManager());
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(mAppSectionsPagerAdapter);
+        pager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                getActionBar().setSelectedNavigationItem(position);
+            }
+        });
+
+        for (int i = 0; i < mAppSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by the adapter.
+            // Also specify this Activity object, which implements the TabListener interface, as the
+            // listener for when this tab is selected.
+            getActionBar().addTab(
+                    getActionBar().newTab()
+                            .setText(mAppSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        pager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
     }
 }
