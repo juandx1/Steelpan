63c86993a67f9b86266a2c1f90008cc0a57014c7
juandx1 <juandx1@hotmail.com>
2014-10-28 12:29:31 -0500

    Base de datos, Animaciones, Game activity

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,6 +1,6 @@
 # Add project specific ProGuard rules here.
 # By default, the flags in this file are appended to flags specified
-# in C:\Users\Juan Manuel\AppData\Local\Android\android-studio\sdk/tools/proguard/proguard-android.txt
+# in c:\Users\Juan Manuel\AppData\Local\Android\android-studio\sdk/tools/proguard/proguard-android.txt
 # You can edit the include path and order by changing the proguardFiles
 # directive in build.gradle.
 #
--- app/src/main/java/com/stigmasoft/matap/steelpan/SongsIncompleteActivity.java
+++ app/src/main/java/com/stigmasoft/matap/steelpan/SongsIncompleteActivity.java
@@ -1,19 +1,50 @@
 package com.stigmasoft.matap.steelpan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Juan Manuel on 09/10/2014.
  */
 public class SongsIncompleteActivity extends Fragment {
 
+    private ArrayList<String> myStringArray1 ;
+
+    public static ArrayAdapter<String> adapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.songs_activity, container, false);
+        final View root = inflater.inflate(R.layout.songs_activity, container, false);
+
+        final ListView listView = (ListView) root.findViewById(R.id.listViewSongs);
+
+        myStringArray1 =  new ArrayList<String>();
+        myStringArray1.add("sdas");
+        myStringArray1.add("asdas");
+        myStringArray1.add("iudj");
+
+        adapter = new ArrayAdapter<String>(getActivity(), R.layout.row_list_view, R.id.textViewSongName, myStringArray1);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                Intent intent = new Intent(getActivity(), GameActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
         return root;
     }
 }
